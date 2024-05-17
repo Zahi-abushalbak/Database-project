6ef16715c7a0a3915380b0c2b96189aa56461e2c
Zahi-abushalbak <133583663+Zahi-abushalbak@users.noreply.github.com>
2024-05-18 01:02:35 +0300

    Add files via upload

---
+++ ZahiAndHadiProject.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.zahiandhadiproject;
+
+import javax.swing.*;
+import static javax.swing.WindowConstants.*;
+
+/**
+ *
+ * @author ramin
+ */
+public class ZahiAndHadiProject {
+
+    public static void main(String[] args) {
+        
+        
+        JFrame frame = new zahi();
+       frame.setVisible(true);
+       frame.setSize(1320, 920);
+       frame.setResizable(false);
+       frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
